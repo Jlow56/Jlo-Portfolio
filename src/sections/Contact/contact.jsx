223c82476dc13bf306952f3a9138ae9ede590a72
Jlow56 <jeanlougames@hotmail.com>
2025-07-01 18:01:21 +0200

    contact

--- src/sections/Contact/contact.jsx
+++ src/sections/Contact/contact.jsx
@@ -1,22 +1,25 @@
  
 const Contact = () => {
     return (
-        <>
-        <section className="contact" id="contact"  aria-label="section contact du portfolio">
+        <section className="contact" id="contact" aria-label="section contact du portfolio">
             <h2>Contact</h2>
-            <form action="" method="post">
-                <label htmlFor="">Nom</label>
-                <input type="text" name="name" id="name" />
-                
-                <label htmlFor="">Prénom</label>
-                <input type="text" name="" id="" />
-                
+            <form method="post" onSubmit={(e) => e.preventDefault()}>
+                <label htmlFor="name">Nom</label>
+                <input type="text" name="name" id="name" required />
+
+                <label htmlFor="firstname">Prénom</label>
+                <input type="text" name="firstname" id="firstname" required />
+
                 <label htmlFor="email">Email</label>
-				<input type="email" name="email" id="email"></input>
+                <input type="email" name="email" id="email" required />
+
+                <label htmlFor="message">Message</label>
+                <textarea name="message" id="message" rows="5" required />
+
+                <button type="submit">Envoyer</button>
             </form>
         </section>
-        </>
     );
-}
+};
 
 export default Contact;
